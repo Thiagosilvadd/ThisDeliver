e4f884b34f45d0a3d27c5a5e7759828bc65c0c3f
thiagodd <silvaa.thiagod2@gmail.com>
2022-09-06 11:02:22 -0300

    Project created

---
+++ backend/src/main/java/com/thiagodev/ThisDeliver/ThisDeliverApplication.java
@@ -0,0 +1,13 @@
+package com.thiagodev.ThisDeliver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThisDeliverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThisDeliverApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/thiagodev/ThisDeliver/ThisDeliverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thiagodev.ThisDeliver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThisDeliverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
