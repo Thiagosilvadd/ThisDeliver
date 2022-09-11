550856c59a360b49a043bb3d241cd79a8e6fee32
thiagodd <silvaa.thiagod2@gmail.com>
2022-09-11 10:48:21 -0300

    POST /orders

--- backend/src/main/java/com/thiagodev/ThisDeliver/controllers/OrderController.java
+++ backend/src/main/java/com/thiagodev/ThisDeliver/controllers/OrderController.java
@@ -4,13 +4,17 @@ import com.thiagodev.ThisDeliver.dto.OrderDTO;
 import com.thiagodev.ThisDeliver.dto.ProductDTO;
 import com.thiagodev.ThisDeliver.services.OrderService;
 import com.thiagodev.ThisDeliver.services.ProductService;
+import java.net.URI;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 @RestController
 @RequestMapping(value = "/orders")
@@ -23,6 +27,14 @@ public class OrderController {
     public ResponseEntity<List<OrderDTO>> findAll(){
         List<OrderDTO> list = service.findAll();
         return ResponseEntity.ok().body(list);
+    }
+
+    @PostMapping
+    public ResponseEntity<OrderDTO> insert(@RequestBody OrderDTO orderDTO){
+        orderDTO = service.insert(orderDTO);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{id}").buildAndExpand(orderDTO.getId()).toUri();
+
+        return ResponseEntity.created(uri).body(orderDTO);
     }
 
 }
--- backend/src/main/java/com/thiagodev/ThisDeliver/services/OrderService.java
+++ backend/src/main/java/com/thiagodev/ThisDeliver/services/OrderService.java
@@ -1,15 +1,19 @@
 package com.thiagodev.ThisDeliver.services;
 
 import com.thiagodev.ThisDeliver.dto.OrderDTO;
+import com.thiagodev.ThisDeliver.dto.ProductDTO;
 import com.thiagodev.ThisDeliver.entities.Order;
+import com.thiagodev.ThisDeliver.entities.OrderStatus;
+import com.thiagodev.ThisDeliver.entities.Product;
 import com.thiagodev.ThisDeliver.repositories.OrderRepository;
 import com.thiagodev.ThisDeliver.repositories.ProductRepository;
+import java.time.Instant;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
-import java.util.List;
-import java.util.stream.Collectors;
 
 @Service
 public class OrderService {
@@ -17,12 +21,37 @@ public class OrderService {
     @Autowired
     private OrderRepository repository;
 
+    @Autowired
+    private ProductRepository productRepository;
+
 
     @Transactional(readOnly = true)
-    public List<OrderDTO> findAll(){
+    public List<OrderDTO> findAll() {
         List<Order> list = repository.findOrderWithProducts();
-
         return list.stream().map(OrderDTO::new).collect(Collectors.toList());
-
     }
+
+    @Transactional(readOnly = true)
+    public OrderDTO findById(Long id){
+        List<Order> order = repository.findById(id);
+        OrderDTO orderDTO = new OrderDTO(order);
+        return orderDTO;
+    }
+
+    @Transactional
+    public OrderDTO insert(OrderDTO orderDTO){
+        Order order = new Order(null, orderDTO.getAddress(), orderDTO.getLatitude(), orderDTO.getLongitude(), Instant.now(), OrderStatus.PENDING);
+
+        for (ProductDTO p : orderDTO.getProducts()){
+            Product product = productRepository.getOne(p.getId());
+            order.getProducts().add(product);
+        }
+
+        order = repository.save(order);
+
+        return new OrderDTO(order);
+    }
+
+
+
 }
